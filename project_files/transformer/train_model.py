1d7650a4ebb6e492db88f36e33ef3db4e87d691d
chengharv-pen <chengharvp@gmail.com>
2025-11-24 14:47:56 -0500

    making the call to remove transformer directory, seems redundant since it's just CNN transformer without anything in the CNN parameters

--- project_files/transformer/train_model.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-from transformer_trainer import BrainToText_Trainer
-
-args = OmegaConf.load('transformer_args.yaml')
-trainer = BrainToText_Trainer(args)
-metrics = trainer.train()
